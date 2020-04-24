b12bc889579a10e63eb431ca7ee3834d08ae4af0
Aleksandr Tolstoy <aleksandr13tolstoy@gmail.com>
2020-04-24 15:14:31 +0300

    Little refactor

--- autocorrelation/main.py
+++ autocorrelation/main.py
@@ -2,38 +2,29 @@ from typing import Union
 
 import matplotlib.pyplot as plt
 
-from autocorrelation_research import algs
+from signal.create import *
+from autocorrelation import algs
 
-harmonics: int = 10
-omega: int = 1100
-parts: int = 256
-
-range_min: Union[int, float] = 0
-range_max: Union[int, float] = 1
-
-x_gen = algs.signal(harmonics, omega, range_min, range_max)
-y_gen = algs.signal(harmonics, omega, range_min, range_max)
-
-max_tau: int = parts
+MAX_TAU: int = PARTS
 
 
 def show_signals():
-    sig_x = [x_gen(i) for i in range(parts)]
-    sig_y = [y_gen(i) for i in range(parts)]
+    sig_x = [x_gen(i) for i in range(PARTS)]
+    sig_y = [y_gen(i) for i in range(PARTS)]
 
-    plt.plot(range(parts), sig_x, label='first')
-    plt.plot(range(parts), sig_y, label='second')
+    plt.plot(range(PARTS), sig_x, label='first')
+    plt.plot(range(PARTS), sig_y, label='second')
     plt.title('Random signals')
     plt.legend()
     plt.show()
 
 
 def show_autocorrelation():
-    Rxx = [algs.autocorrelation(x_gen, x_gen, parts, tau) for tau in range(max_tau)]
-    Rxy = [algs.autocorrelation(x_gen, y_gen, parts, tau) for tau in range(max_tau)]
+    Rxx = [algs.autocorrelation(x_gen, x_gen, PARTS, tau) for tau in range(MAX_TAU)]
+    Rxy = [algs.autocorrelation(x_gen, y_gen, PARTS, tau) for tau in range(MAX_TAU)]
 
-    plt.plot(range(max_tau), Rxx, label='Rxx(t, tau)')
-    plt.plot(range(max_tau), Rxy, label='Rxy(t, tau)')
+    plt.plot(range(MAX_TAU), Rxx, label='Rxx(t, tau)')
+    plt.plot(range(MAX_TAU), Rxy, label='Rxy(t, tau)')
     plt.title('Autocorrelation')
     plt.legend()
     plt.show()
