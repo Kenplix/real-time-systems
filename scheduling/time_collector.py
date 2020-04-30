a9294d315e0d261522dd79e46f431a2f1b3edb77
Aleksandr Tolstoy <aleksandr13tolstoy@gmail.com>
2020-05-01 00:58:58 +0300

    Implemented time collector

--- scheduling/time_collector.py
+++ scheduling/time_collector.py
@@ -1,17 +1,37 @@
+import json
+
 from timers import timer
-from logged import logged
 
+from signal.main import *
+from autocorrelation.main import autocorr
+from fourier_transform.main import w_table
 
-def test(a, b):
-    return a + b
+from fermat_factor.main import full_factor
 
 
 @timer
 @logged(separator='\n')
-def collector(func, *args, reps=100, **kwargs) -> float:
+def collector(func, *args, reps=1000, **kwargs) -> float:
     for i in range(reps):
         func(*args, **kwargs)
 
 
 if __name__ == '__main__':
-    print(collector(test, 5, 54, reps=100000))
+    x_gen = generator(HARMONICS, FREQUENCY)
+    y_gen = generator(HARMONICS, FREQUENCY)
+
+    sig_x = np.array([x_gen(lag) for lag in LAGS])
+    sig_y = np.array([y_gen(lag) for lag in LAGS])
+
+    REPS = 100000
+
+    autocorr_mean_time = collector(autocorr, sig_x, sig_y, reps=REPS) / REPS
+    dft_mean_time = collector(np.matmul, w_table(len(LAGS)), sig_x, reps=REPS) / REPS
+    factor_mean_time = collector(full_factor, 12345, reps=REPS) / REPS
+
+    average_values = {'autocorrelation': autocorr_mean_time,
+                      'fourier_transform': dft_mean_time,
+                      'fermat_factor': factor_mean_time}
+
+    with open("scheduling/average_values.json", "w") as file:
+        json.dump(average_values, file)
