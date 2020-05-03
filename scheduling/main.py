9b6e2a445a082342357f0291d09eaa38f2adaff4
Aleksandr Tolstoy <aleksandr13tolstoy@gmail.com>
2020-05-03 20:58:02 +0300

    Implemented REAL-TIME in the system and fixed all errors * Rewritten on concurrency

--- scheduling/main.py
+++ scheduling/main.py
@@ -1,7 +1,8 @@
-import logging
 import time
-import threading as th
+import logging
+from threading import Lock
 from queue import PriorityQueue
+from concurrent.futures import ThreadPoolExecutor
 from dataclasses import dataclass
 from collections import namedtuple
 
@@ -14,10 +15,10 @@ from signal.main import *
 from termcolor import colored
 
 REPS: int = 100
-REQUESTS: int = 200
+REQUESTS: int = 1000
 MIN_PRIORITY: int = 3
 MAX_PRIORITY: int = 0
-INTENSITY: float = 40
+INTENSITY: float = 50
 DELAY: float = 1 / INTENSITY
 
 CFG = namedtuple('CFG', ['func', 'params'])
@@ -28,6 +29,7 @@ qsize_counter: int = 0
 current_time: float = 0
 waiting_time: float = 0
 processed_requests: int = 0
+start_time = time.time()
 
 
 @timer
@@ -44,7 +46,7 @@ def build_funcs(*cfgs: CFG) -> Dict[int, CFG]:
     return {index: cfg for index, cfg in enumerate(cfgs)}
 
 
-def dead_coef(min: int, max: int):
+def dead_coef(min: int, max: int) -> float:
     if min < 0 or min > max:
         raise ValueError
     return 1 + (random.randint(min, max) * random.random())
@@ -65,73 +67,55 @@ class Request:
         return self.deadline < other.deadline
 
     def __repr__(self):
-        return f'{self.deadline=}'
+        return f'{self.login=}, {self.deadline=}'
 
 
-class ProducerThread(th.Thread):
-    def __init__(self, name, queue, average: Dict[int, float], lock):
-        super().__init__()
-        self.name = name
-        self.queue = queue
-        self.average = average
-        self.lock = lock
+def producer(queue, average: Dict[int, float], lock) -> None:
+    global qsize_counter
+    request_number = 0
+    producer_time = time.time() - start_time
+    while request_number < REQUESTS:
+        priority = random.randint(MAX_PRIORITY, MIN_PRIORITY)
+        item = Request(id := random.randint(0, 2), producer_time, average[id])
+        queue.put((priority, item))
+        time.sleep(DELAY)
+        prefix = colored('Putting', 'cyan', attrs=['bold'])
+        logging.debug(f'{prefix} {current_time=} {item}')
 
-    def run(self):
-        global qsize_counter
-        request_number, producer_time = 0, 0
-        while request_number < REQUESTS:
-            priority = random.randint(MAX_PRIORITY, MIN_PRIORITY)
-            item = Request(id := random.randint(0, 2), producer_time, self.average[id])
-            self.queue.put((priority, item))
-
-            with self.lock:
-                qsize_counter += self.queue.qsize()
-            time.sleep(DELAY)
-
-            prefix = colored('Putting', 'cyan', attrs=['bold'])
-            logging.debug(f'{prefix} {current_time} {item}')
-            producer_time += DELAY
-            request_number += 1
+        with lock:
+            qsize_counter += queue.qsize()
+        producer_time += DELAY
+        request_number += 1
 
 
-class ConsumerThread(th.Thread):
-    def __init__(self, name, queue, funcs: Dict[int, CFG], lock):
-        super().__init__()
-        self.name = name
-        self.queue = queue
-        self.funcs = funcs
-        self.lock = lock
+def consumer(queue, funcs: Dict[int, CFG], lock) -> None:
+    global current_time, waiting_time
+    global processed_requests
+    request_number = 0
+    while request_number < REQUESTS:
+        if not queue.empty():
+            priority, item = queue.get()
+            with lock:
+                waiting_time += time.time() - start_time - item.login
 
-    def run(self):
-        global current_time, waiting_time
-        global processed_requests
-        request_number = 0
-        start_time = time.time()
-        while request_number < REQUESTS:
-            if not self.queue.empty():
-                priority, item = self.queue.get()
-                with self.lock:
-                    waiting_time += time.time() - start_time - item.login
+            is_executed = False
+            if time.time() - start_time < item.deadline:
+                cfg = funcs[item.task_id]
+                cfg.func(*cfg.params)
+                is_executed = True
+            queue.task_done()
 
-                is_executed = False
-                if time.time() - start_time < item.deadline:
-                    cfg = self.funcs[item.task_id]
-                    cfg.func(*cfg.params)
-                    is_executed = True
-
-                prefix = colored('Failed', 'red', attrs=['bold'])
-                with self.lock:
-                    current_time = time.time() - start_time
-                    if current_time < item.deadline and is_executed:
-                        processed_requests += 1
-                        prefix = colored('Passed', 'green', attrs=['bold'])
-                    request_number += 1
-
-                logging.debug(f'{prefix} {current_time=} {item}')
-                self.queue.task_done()
+            prefix = colored('Failed', 'red', attrs=['bold'])
+            with lock:
+                current_time = time.time() - start_time
+                if current_time < item.deadline and is_executed:
+                    processed_requests += 1
+                    prefix = colored('Passed', 'green', attrs=['bold'])
+                request_number += 1
+            logging.debug(f'{prefix} {current_time=} {item}')
 
 
-def main(*, max_workers: int = 1):
+def main(*, delay: float = 0, buf_size: Optional[int] = None, max_workers: int = 1):
     x_gen = generator(HARMONICS, FREQUENCY)
     y_gen = generator(HARMONICS, FREQUENCY)
 
@@ -143,32 +127,24 @@ def main(*, max_workers: int = 1):
     c3 = CFG(full_factor, [12345])
     configs = (c1, c2, c3)
 
-    queue = PriorityQueue()
+    queue = PriorityQueue(buf_size) if buf_size else PriorityQueue()
     average = calculate_average(*configs, reps=REPS)
-    lock = th.Lock()
-    producer = ProducerThread('producer', queue, average, lock)
-    producer.start()
-
+    lock = Lock()
     funcs = build_funcs(*configs)
-    for _ in range(max_workers):
-        consumer = ConsumerThread('consumer', queue, funcs, lock)
-        consumer.start()
-        consumer.join()
 
-    producer.join()
+    with ThreadPoolExecutor(max_workers) as executor:
+        executor.submit(producer, queue, average, lock)
+        time.sleep(delay)
+        executor.submit(consumer, queue, funcs, lock)
 
-    print(f'Requests {REQUESTS}\n'
-          f'Workers {max_workers}')
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG, format='%(message)s')
+    main(delay=0.2, max_workers=3)
+    print(f'Requests {REQUESTS}')
     print(f'Average qsize {qsize_counter / REQUESTS}\n'
           f'Average waiting {waiting_time / REQUESTS}')
 
     overdue_requests = REQUESTS - processed_requests
     print(f'Number of overdue requests {overdue_requests}\n'
           f'Overdue requests / requests {overdue_requests/REQUESTS}')
-
-
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG,
-                        format='(%(threadName)s) %(message)s')
-
-    main()
